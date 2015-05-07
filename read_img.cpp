1a2c41d519dd7b67638ecfaa4b3d29aec9addf22
Tiago Royer <royertiago@gmail.com>
2015-05-07 17:22:24 -0300

    read_img.cpp: Properly throw std::runtime_error.

--- read_img.cpp
+++ read_img.cpp
@@ -1,8 +1,12 @@
+#include <stdexcept>
 #include <opencv2/highgui/highgui.hpp>
 #include "read_img.h"
 
 std::vector<float> read_pixels( std::string filename ) {
     cv::Mat_<float> img = cv::imread( filename, CV_LOAD_IMAGE_GRAYSCALE );
+
+    if( ! img.data )
+        throw std::runtime_error("Error reading file " + filename);
 
     std::vector<float> ret(img.begin(), img.end());
     for( auto & f : ret )
